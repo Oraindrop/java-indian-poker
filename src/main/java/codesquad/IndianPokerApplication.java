8faedfcef99d4a5f1d7271f715bf5f222b5ef2bd
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2019-01-14 01:38:00 +0900

    first commit

---
+++ src/main/java/codesquad/IndianPokerApplication.java
@@ -0,0 +1,11 @@
+package codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianPokerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndianPokerApplication.class);
+    }
+}
